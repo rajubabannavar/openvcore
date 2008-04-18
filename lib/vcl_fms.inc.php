c42ffb02b4e4693892915f6e6c4a5faf8e70873b
teh.cmn <teh.cmn@b65af105-244b-0410-896f-0db2c82a080e>
2008-04-18 20:15:19 +0000

    initial (broken) SWF generation implemented in the FMS module

--- lib/vcl_fms.inc.php
+++ lib/vcl_fms.inc.php
@@ -10,6 +10,12 @@
 		private $fms_server = '';
 		private $fms_source = '';
 
+		private $as = '';		//Actionscript
+
+		function __construct(){
+			define('CRLF',"\r\n");
+		}
+
 		private function generate_fms_source(){
 			if(!$fms_schema 
 				or !$fms_server 
@@ -31,6 +37,33 @@
 
 		public function ToString(){
 			return generate_fms_source();
+		}
+
+		public function GenerateSWF($width, $height, $backcolor){
+			$fm = new SWFMovie(7);
+
+			$fm->setDimension($width,$height);
+			$fm->setBackground($backcolor['r'],$backcolor['g'],$backcolor['b']);
+			
+			$vds = new SWFVideoStream();
+			$vds->setDimension($width,$height);
+
+			$sfo_FLVContainer0 = $fm->add($vds);
+			$sfo_FLVContainer0->moveTo(0,0);
+			$sfo_FLVContainer0->setname('FLVContainer0');
+
+			$this->as .= 'nc = new NetConnection();' . CRLF;
+			$this->as .= 'nc.connect(null);' . CRLF;
+			$this->as .= 'stream = new NetStream(nc);' . CRLF;
+			$this->as .= 'FLVContainer0.attachVideo(stream);' . CRLF;
+			$this->as .= 'stream.setBufferTime(10);' . CRLF;
+			$this->as .= 'stream.play(\'' . $this->generate_fms_source() . '\');';
+
+			$aso = new SWFAction($this->as);
+			$fm->add($aso);
+
+			header('Content-type: application/x-shockwave-flash');
+			$fm->output();
 		}
 	}
 
