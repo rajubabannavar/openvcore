8ca1bb67d7a4006374cf84e59538f0f4a25763b1
teh.cmn <teh.cmn@b65af105-244b-0410-896f-0db2c82a080e>
2008-04-19 18:20:22 +0000

    - basic user support added

--- trunk/lib/vcl_user.inc.php
+++ trunk/lib/vcl_user.inc.php
@@ -13,26 +13,50 @@
 	class User {
 
 		public $UserData = Array();
-		private $dbo = null;
+		protected $dbo = null;
 
 		function __construct($username = ''){
 			global $config;
+			$dyn_classname = $config['db_type'] . 'DB';
+			$this->dbo = new $dyn_classname();
+			$this->dbo->Connect(
+				$config['db_host'],
+				$config['db_user'],
+				$config['db_password'],
+				$config['db_schema']
+			);
+
 			if(!empty($username)){
 				//get user data
-				$dyn_classname = $config['db_type'] . 'DB';
-				$this->dbo = new $dyn_classname();
-				$this->dbo->Connect(
-					$config['db_host'],
-					$config['db_user'],
-					$config['db_password'],
-					$config['db_schema']
-				);
 				$this->dbo->Query('SELECT * FROM vc_users WHERE user_name = "' .
 					mysql_escape_string($username) . '"');
 				$this->UserData = $this->dbo->ResultSet[0];
 			}
 		}
-		
+
+		function Create(){
+			global $config;
+			$required_fields = Array(
+				'user_name',
+				'user_password',
+				'user_first_name',
+				'user_last_name'
+			);
+
+			foreach($required_fields as $field){
+				if(!$this->UserData[$field]){
+					throw new Exception('Required userdata field ' . $field . ' missing');
+				}else{
+					$this->UserData[$field] = mysql_escape_string($this->UserData[$field]);
+					if(is_string($this->UserData[$field])){
+						$this->UserData[$field] = "'{$this->UserData[$field]}'";
+					}
+				}
+			}
+
+			$this->dbo->Query('INSERT INTO vc_users (' . implode(',',array_keys($this->UserData)) . ')
+				VALUES (' . implode(',',$this->UserData) . ')');
+		}
 	}
 
 ?>
